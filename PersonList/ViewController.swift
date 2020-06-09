fce30846f9cbc3275c6b6bd995699b336d35fa86
kramarkirill <kkramar@kantiana.ru>
2020-06-09 09:19:45 +0200

    Initial Commit

---
+++ PersonList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PersonList
+//
+//  Created by Кирилл Крамар on 09.06.2020.
+//  Copyright © 2020 Кирилл Крамар. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
